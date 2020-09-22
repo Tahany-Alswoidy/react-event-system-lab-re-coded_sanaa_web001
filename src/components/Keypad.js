ff18f959ba57f6d4473711c99a463fbf41ad0809
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-22 04:45:53 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+import React from 'react';
+
+class Keypad extends Component{
+    render(){
+        return(
+            <input type="password" onKeyUp={}/>
+        )
+    }
+}
+
+export default Keypad;
